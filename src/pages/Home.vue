eac520aa1226442a86b1d62b0ea45e9019d2b7ba
smcfadden150 <smcfadden150@gmail.com>
2020-07-14 05:44:12 -0400

    Added Concet Art :art: and Updated Home Page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,8 +5,32 @@
       <div class="main-container"> 
         <div class="main">
           <div class="game-description">
-            In a world choked by pollution and robbed of resources, wage a one-man war on what remains of the greedy nations and their robotic robbers. 
-            Ignite your ship and use the surrounding smog to blast through your enemies! 
+
+            <i>In a world choked by pollution and robbed of resources, wage a one-man war on what remains of the greedy nations and their robotic robbers. 
+            Ignite your ship and use the surrounding smog to blast through your enemies! </i>
+
+            <br/><br/><br/>
+
+            <h3>What is Oil and Cordite?</h3>
+            Oil and Cordite is an action packed flight game currently in development. The game features fast paced action and a intuitive control system. This should probably be replaced with some cool trailers or gameplact screen shots! 
+            
+            <br/><br/><br/>
+
+            <h3>What Engine is Oil and Cordite Using?</h3>
+            Oil and Cordite is built using the Unity engine.
+            
+            <br/><br/><br/>
+
+            <h3>What Platforms Support Oil and Cordite?</h3>
+            Oil and Cordite currently only supports PC, although a Switch port may be possible.
+            
+            <br/><br/><br/>
+
+            <h3>Why is this Homepage So Barren?</h3>
+            Because the cool interactive three.js homepage is still in progress. It should be finished soon.
+            
+            <br/><br/><br/>
+
           </div>
         </div>
       </div>
