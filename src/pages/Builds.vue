4e6ccf0390b7ffb4a2ab03100ec7477b27e4160b
smcfadden150 <smcfadden150@gmail.com>
2020-07-14 06:08:10 -0400

    Added Information to Build Page

--- src/pages/Builds.vue
+++ src/pages/Builds.vue
@@ -4,7 +4,18 @@
     <transition name="fade" appear="true">
       <div class="main-container"> 
         <div class="main">
-          <HelloWorld msg="Welcome to Your Vue.js App"/>
+          <div class="team-header">The Game</div>
+          <div class="game-description">
+            Using the button below, you can download the most recent playable release of Oil and Cordite. Additionally, you can find patch notes below. 
+          </div>
+          <div class="download-container">
+            <b-button variant="outline-primary" class="download-button"> Download! </b-button>
+          </div>
+          <br/><br/><br/>
+          <div class="game-description">
+            <h3>Patch Notes:</h3>
+              <i> There are no releases for this project at the moment. Patch notes are updated when a release is scheduled through GitHub.</i>
+          </div>
         </div>
       </div>
     </transition>
@@ -12,9 +23,14 @@
 </template>
 
 <script>
-import '../scss/style.scss';
 import Header from '../components/Header.vue';
 import HelloWorld from '../components/HelloWorld.vue';
+
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '../scss/style.scss';
+import { BButton } from 'bootstrap-vue';
 
 export default {
   name: 'App',
